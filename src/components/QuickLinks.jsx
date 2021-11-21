cff919435481c9683921b02cc38d6648b3dc088a
Jason <jasonli.0616@outlook.com>
2021-11-21 01:24:51 -0500

    Add GitHub logo

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./../css/QuickLinks.css";
 import ClassroomIcon from "./../images/classroom_icon.png";
+import GithubIcon from "./../images/github_icon.png";
 import DiscordIcon from "./../images/discord_icon.png";
 import InstagramIcon from "./../images/instagram_icon.png";
 import GWSSLogo from "./../images/gwss_logo.png";
@@ -40,29 +41,28 @@ export default class QuickLinks extends Component {
                 <div className="quicklinks-imagelink">
                     <ImageLink url="http://bit.ly/gwsics2020" image={ClassroomIcon} alt="Google Classroom"
                         data-aos-delay="300"
-                        data-aos-duration="700"
+                    />
+                    <ImageLink url="https://github.com/hdsb-gws-programmingclub" image={GithubIcon} alt="GitHub"
+                        data-aos="zoom-out-down"
+                        data-aos-delay="500"
                     />
                     <ImageLink url="https://discord.gg/S6NAzfdfYg" image={DiscordIcon} alt="Discord"
                         data-aos="zoom-out-down"
-                        data-aos-delay="500"
-                        data-aos-duration="700"
+                        data-aos-delay="700"
                     />
                     <ImageLink url="https://www.instagram.com/gwss_programming_club/" image={InstagramIcon} alt="Instagram"
                         data-aos="zoom-out-down"
-                        data-aos-delay="700"
-                        data-aos-duration="700"
+                        data-aos-delay="900"
                     />
                     <ImageLink url="https://gws.hdsb.ca/" image={GWSSLogo} alt="GWSS"
                         data-aos="zoom-out-down"
-                        data-aos-delay="900"
-                        data-aos-duration="700"
+                        data-aos-delay="1100"
                     />
                 </div>
                 <div className="quicklinks-textlink">
                     <TextLink url="https://classroom.google.com/c/MjM1Mjk3MjczNTUy/m/MjcwODE2ODYxMDQ1/details"
                         data-aos="zoom-out-down"
-                        data-aos-delay="1100"
-                        data-aos-duration="700"
+                        data-aos-delay="1300"
                     >Info for new joiners</TextLink>
                 </div>
             </>
