833d6b47629cdd16594edae29a05b53f4e0ea025
Jason <jasonli.0616@outlook.com>
2021-11-23 18:38:37 -0500

    Update animations

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -25,7 +25,7 @@ class ImageLink extends Component {
 class TextLink extends Component {
     render() {
         return (
-            <Button url={this.props.url} data-aos="zoom-out-down" data-aos-delay="1000" data-aos-duration="700" data-aos-anchor="top-bottom">{this.props.children}</Button>
+            <Button url={this.props.url} data-aos="zoom-out-down" data-aos-delay={this.props['data-aos-delay']} data-aos-duration="700" data-aos-anchor="top-bottom">{this.props.children}</Button>
         );
     };
 };
@@ -35,30 +35,18 @@ export default class QuickLinks extends Component {
         return (
             <>
                 <div className="quicklinks-imagelink">
-                    <ImageLink url="http://bit.ly/gwsics2020" image={ClassroomIcon} alt="Google Classroom"
-                        data-aos-delay="300"
-                    />
-                    <ImageLink url="https://github.com/hdsb-gws-programmingclub" image={GithubIcon} alt="GitHub"
-                        data-aos-delay="500"
-                    />
-                    <ImageLink url="https://discord.gg/S6NAzfdfYg" image={DiscordIcon} alt="Discord"
-                        data-aos-delay="700"
-                    />
-                    <ImageLink url="https://www.youtube.com/channel/UCRQykAZN3cenPXuVbTqdxgw" image={YouTubeIcon} alt="YouTube"
-                        data-aos-delay="900"
-                    />
-                    <ImageLink url="https://www.instagram.com/gwss_programming_club/" image={InstagramIcon} alt="Instagram"
-                        data-aos-delay="1100"
-                    />
-                    <ImageLink url="https://gws.hdsb.ca/" image={GWSSLogo} alt="GWSS"
-                        data-aos-delay="1300"
-                    />
+                    <ImageLink url="http://bit.ly/gwsics2020" image={ClassroomIcon} alt="Google Classroom" data-aos-delay="300" />
+                    <ImageLink url="https://github.com/hdsb-gws-programmingclub" image={GithubIcon} alt="GitHub" data-aos-delay="500" />
+                    <ImageLink url="https://discord.gg/S6NAzfdfYg" image={DiscordIcon} alt="Discord" data-aos-delay="700" />
+                    <ImageLink url="https://www.youtube.com/channel/UCRQykAZN3cenPXuVbTqdxgw" image={YouTubeIcon} alt="YouTube" data-aos-delay="900" />
+                    <ImageLink url="https://www.instagram.com/gwss_programming_club/" image={InstagramIcon} alt="Instagram" data-aos-delay="1100" />
+                    <ImageLink url="https://gws.hdsb.ca/" image={GWSSLogo} alt="GWSS" data-aos-delay="1300" />
                 </div>
                 <h2 style={{textAlign: 'center'}}>Resources:</h2>
                 <div className="quicklinks-textlink">
-                    <TextLink url="https://classroom.google.com/c/MjM1Mjk3MjczNTUy/m/MjcwODE2ODYxMDQ1/details">Info for new joiners</TextLink>
-                    <TextLink url="https://drive.google.com/drive/folders/1ZtOD0Hr1nDZwdGoIllxFcvWzTMAsvcba?usp=sharing">Learn to code</TextLink>
-                    <TextLink url="https://drive.google.com/drive/folders/1svprXe5s60OLeZ9CVQkMSkXn4W2wfUef?usp=sharing">Past programming challenges</TextLink>
+                    <TextLink url="https://classroom.google.com/c/MjM1Mjk3MjczNTUy/m/MjcwODE2ODYxMDQ1/details" data-aos-delay="400">Info for new joiners</TextLink>
+                    <TextLink url="https://drive.google.com/drive/folders/1ZtOD0Hr1nDZwdGoIllxFcvWzTMAsvcba?usp=sharing" data-aos-delay="800">Learn to code</TextLink>
+                    <TextLink url="https://drive.google.com/drive/folders/1svprXe5s60OLeZ9CVQkMSkXn4W2wfUef?usp=sharing" data-aos-delay="1200">Past programming challenges</TextLink>
                 </div>
             </>
         );
