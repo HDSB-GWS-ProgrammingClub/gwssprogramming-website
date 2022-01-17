7db97a86884f08c95f7ff739a15016734e9765d5
Jason <jasonli.0616@outlook.com>
2022-01-17 16:34:43 -0500

    Remove alert

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,13 +7,6 @@ export default class Title extends Component {
     render() {
         return (
             <>
-                <div>
-                    <Alert>
-                        The Ontario government has annnounced: "All publicly funded and private schools will move to remote learning starting January 5 until at least January 17, subject to public health trends and operational considerations." <a href="https://news.ontario.ca/en/release/1001394/ontario-temporarily-moving-to-modified-step-two-of-the-roadmap-to-reopen" target="_blank" rel="noreferrer">Click here</a> for more information.
-                        <br /><br />
-                        Check the club Google Classroom for updates on meetings during remote learning.
-                    </Alert>
-                </div>
                 <div className="title">
                     <img src={GWPCLogo} alt="GWPC Logo" />
                     <h1>Garth Webb Programming Club</h1>
